3007036621f47beeff80709a09f04a2442232914
igorsid <sidgarri@gmail.com>
2022-07-15 21:16:48 +0800

    fix

--- src/test/java/tests/WikiTests.java
+++ src/test/java/tests/WikiTests.java
@@ -10,19 +10,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class WikiTests extends TestBase {
-    
-    @Test
-    @DisplayName("Search BrowserStack")
-    void searchTest() {
-        back();
-        step("Type search", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
-        });
-        step("Verify content found", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(CollectionCondition.sizeGreaterThan(0)));
-    }
 
     @Test
     @DisplayName("Open Article")
